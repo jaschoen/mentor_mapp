2dd57393adc95683fc622e250baf9c4908be7675
Justin Schoen <jaschoen@gmail.com>
2015-11-09 17:25:40 -0600

    semi-working profiles

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151109203539) do
+ActiveRecord::Schema.define(version: 20151109230332) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "contacts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "junks", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "city"
+    t.string   "state"
+    t.string   "coach"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
